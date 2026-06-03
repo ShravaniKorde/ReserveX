5c94bf0c86c249788743b3feac7f1536b7aab055
Shravani Korde <shravani.korde11@gmail.com>
2026-06-03 15:38:03 +0530

    Initial springboot setup

---
+++ src/main/java/com/reservex/reservex/ReservexApplication.java
@@ -0,0 +1,13 @@
+package com.reservex.reservex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservexApplication.class, args);
+	}
+
+}
